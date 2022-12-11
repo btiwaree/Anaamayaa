db8dd64f73968dfdf0fbb54eb27435e0c00d9b4c
Bishesh Tiwaree <bisheshtiwaree@live.com>
2022-12-11 13:07:51 +0100

    feat: meta

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="ne">
       <Head>
-        <title>Anamaya</title>
+        <title>Anamaya Ayurved Multi-Specialty Clinic</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -12,6 +12,13 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap"
           rel="stylesheet"
         />
+        <meta
+          name="description"
+          content="Anamaya Ayurved Multi-Specialty Clinic - Biratnagar - First stop in Biratnagar for your Ayurvedic needs - Cheap, Reliable"
+        />
+        <meta name="keywords" content="Ayurved, Anamaya, Clinic, Multi-Specialty, Health" />
+        <meta name="author" content="Anamaya Ayurved Multi-Specialty Clinic" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <body>
         <Main />
